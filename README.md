407c720aa476c9f319fc6f5622d691babaf524e3
Tengeevandu <64324719+OGNamsta@users.noreply.github.com>
2024-02-27 11:47:50 +0200

    Initial commit. Includes all branches, stored in a json as a cache and in a txt file.

---
+++ README.md
@@ -0,0 +1,8 @@
+The purpose of this project is to query the API of DolEX Financial Services to extract a list of branches. More specifically, a list of branches located within the state of California. 
+The specific API endpoint that will be used is the "Branches" endpoint. This endpoint will return a list of branches that are located within the state of California.
+The data that will be extracted from the API will be stored in a pandas dataframe. The dataframe will contain the following columns:
+ - Branch Name
+ - Branch Address
+ - Branch City
+ - Branch State
+ - Branch Zip Code
---
+++ main.py
@@ -0,0 +1,94 @@
+import asyncio
+import httpx
+import json
+from time import perf_counter
+from aiolimiter import AsyncLimiter
+
+
+async def log_request(request):
+    print(f"Request: {request.url!r} {request.method!r}")
+
+
+async def log_response(response):
+    print(f"Response: {response.url!r} {response.status_code!r}")
+
+
+async def get_branch():
+    cookies = {
+        'eael_screen': '1280',
+        'cookielawinfo-checkbox-necessary': 'yes',
+        'cookielawinfo-checkbox-functional': 'no',
+        'cookielawinfo-checkbox-performance': 'no',
+        'cookielawinfo-checkbox-analytics': 'no',
+        'cookielawinfo-checkbox-advertisement': 'no',
+        'cookielawinfo-checkbox-others': 'no',
+        'CookieLawInfoConsent': 'eyJuZWNlc3NhcnkiOnRydWUsImZ1bmN0aW9uYWwiOmZhbHNlLCJwZXJmb3JtYW5jZSI6ZmFsc2UsImFuYWx5dGljcyI6ZmFsc2UsImFkdmVydGlzZW1lbnQiOmZhbHNlLCJvdGhlcnMiOmZhbHNlfQ==',
+        'viewed_cookie_policy': 'yes',
+    }
+
+    headers = {
+        'authority': 'www.dolex.com',
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'accept-language': 'en-US,en;q=0.9,en-GB;q=0.8,de;q=0.7,es;q=0.6',
+        'dnt': '1',
+        'referer': 'https://www.dolex.com/branches-products-services/',
+        'sec-ch-ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+        'x-requested-with': 'XMLHttpRequest',
+    }
+
+    params = {
+        'action': 'asl_load_stores',
+        'lang': '',
+        'nonce': '757027776f',
+        'load_all': '1',
+        'layout': '1',
+    }
+    branch_list = []
+
+    url = 'https://www.dolex.com/wp-admin/admin-ajax.php'
+
+    # Create an AsyncLimiter instance
+    limiter = AsyncLimiter(10)
+    # Use the limiter to limit the number of concurrent requests
+    async with limiter:
+        async with httpx.AsyncClient(event_hooks={'request': [log_request], 'response': [log_response]}, headers=headers, cookies=cookies, timeout=60.0) as client:
+            response = await client.get(url, params=params)
+            if response.status_code == 200:
+                if content_type := response.headers.get('Content-Type', '').lower():
+                    if 'text/html' in content_type:
+                        data = response.json()
+                        # Extract the branch data
+                        branch_list.extend(data)
+                        # Cache the JSON output to a file
+                        with open(f'branch_data.json', 'w') as f:
+                            json.dump(data, f, indent=4)
+                            print(f"Data cached to branch_data.json")
+                    else:
+                        print(f"Received error response: {response.status_code}")
+                else:
+                    print("No Content-Type header received")
+            else:
+                print(f"Received error response: {response.status_code}")
+        # Save the branch_list to a file
+        with open('branch_list.txt', 'w') as f:
+            for branch in branch_list:
+                f.write(f'{branch}\n')
+        # prints the length of the list
+        print(len(branch_list))
+        return branch_list
+
+
+async def main():
+    start = perf_counter()
+    await get_branch()
+    finish = perf_counter()
+    print(f'Finished in {round(finish-start, 2)} second(s)')
+
+if __name__ == '__main__':
+    asyncio.run(main())
